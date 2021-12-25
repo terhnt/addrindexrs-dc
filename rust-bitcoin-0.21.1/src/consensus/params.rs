4f3a3ac1249f193c5c05a9f036b383a7873ca764
Andrew <95168952+terhnt@users.noreply.github.com>
2021-12-26 03:17:25 +1100

    small adjust - need to fix bip heights*
    2016 for miner confirmation window might be correct.

--- rust-bitcoin-0.21.1/src/consensus/params.rs
+++ rust-bitcoin-0.21.1/src/consensus/params.rs
@@ -84,10 +84,10 @@ impl Params {
                 bip65_height: 388381, // 000000000000000004c2b624ed5d7756c508d90fd0da2c7c679febfa6c4735f0
                 bip66_height: 363725, // 00000000000000000379eaa19dce8c9b722d46ae6a57c2f1a988119488b50931
                 rule_change_activation_threshold: 1916, // 95%
-                miner_confirmation_window: 2016,
+                miner_confirmation_window: 3,
                 pow_limit: MAX_BITS_BITCOIN,
-                pow_target_spacing: 10 * 60,            // 10 minutes.
-                pow_target_timespan: 14 * 24 * 60 * 60, // 2 weeks.
+                pow_target_spacing: 60,            // 1 minute
+                pow_target_timespan: 3 * 60, // 3 minutes
                 allow_min_difficulty_blocks: false,
                 no_pow_retargeting: false,
             },
@@ -98,10 +98,10 @@ impl Params {
                 bip65_height: 581885, // 00000000007f6655f22f98e72ed80d8b06dc761d5da09df0fa1dc4be4f861eb6
                 bip66_height: 330776, // 000000002104c8c45e99a8853285a3b592602a3ccde2b832481da85e9e4ba182
                 rule_change_activation_threshold: 1512, // 75%
-                miner_confirmation_window: 2016,
+                miner_confirmation_window: 3,
                 pow_limit: MAX_BITS_TESTNET,
-                pow_target_spacing: 10 * 60,            // 10 minutes.
-                pow_target_timespan: 14 * 24 * 60 * 60, // 2 weeks.
+                pow_target_spacing: 60,            // 1 minute
+                pow_target_timespan: 3 * 60, // 3 minutes
                 allow_min_difficulty_blocks: true,
                 no_pow_retargeting: false,
             },
@@ -112,10 +112,10 @@ impl Params {
                 bip65_height: 1351,
                 bip66_height: 1251,                    // used only in rpc tests
                 rule_change_activation_threshold: 108, // 75%
-                miner_confirmation_window: 144,
+                miner_confirmation_window: 3,
                 pow_limit: MAX_BITS_REGTEST,
-                pow_target_spacing: 10 * 60,            // 10 minutes.
-                pow_target_timespan: 14 * 24 * 60 * 60, // 2 weeks.
+                pow_target_spacing: 60,            // 60 seconds
+                pow_target_timespan: 3 * 60, // 3 minutes
                 allow_min_difficulty_blocks: true,
                 no_pow_retargeting: true,
             },
